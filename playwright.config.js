4114c2bff9444259b15be39aa1ac763301d051cc
HendiliMohamedAziz <83289023+HendiliMohamedAziz@users.noreply.github.com>
2025-10-20 17:57:25 +0200

    Jenkins config

--- playwright.config.js
+++ playwright.config.js
@@ -23,14 +23,21 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html' ], ['line' ], ["allure-playwright"]],
+  timeout: 20*1000,
+  expect:{
+    timeout: 10000,
+  },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+    headless: false,
+    screenshot: 'only-on-failure',
+    video: 'on',
   },
 
   /* Configure projects for major browsers */
